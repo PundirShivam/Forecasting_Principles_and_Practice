6bd87a014b316c319f185d3e6ecd88231b8ec5b0
ngurnani <ngurnani@gmail.com>
2020-08-16 22:06:45 -0400

    add check_residuals, run black formatting on other files

--- utils/cplot.py
+++ utils/cplot.py
@@ -1,4 +1,4 @@
-'''
+"""
 Module for custom plots
 based on the ideas gathered 
 from r ggplot package,
@@ -8,17 +8,18 @@ to these functions.
 @author : shivam pundir
 @date : 3 July 2020
 
-'''
-import pandas as pd 
+"""
+import pandas as pd
 import numpy as np
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-__all__  = ['seasonal']
+__all__ = ["seasonal"]
 
-def seasonal(ts,col_name,figsize=(8,6)):
-    '''
+
+def seasonal(ts, col_name, figsize=(8, 6)):
+    """
     Returns a fig,ax object of seasonal plot
     for the ts time series for given column
     name col_name.
@@ -37,28 +38,44 @@ def seasonal(ts,col_name,figsize=(8,6)):
     ------------
     ------------
     fig, ax = tupple consisting of fig,ax
-    '''
-    f ,ax = plt.subplots(figsize=figsize)
-    ((ts)
-     .assign(year=ts.index.year)
-     .assign(month=ts.index.month)
-     .groupby(['year','month'])
-     .sum().reset_index()
-     .pivot_table(index='month',columns='year',values=col_name)).plot(marker='o',ax=ax)
-    x_ticks = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    plt.xticks(range(1,13),x_ticks)
-    plt.grid(True,alpha=.7)
+    """
+    f, ax = plt.subplots(figsize=figsize)
+    (
+        (ts)
+        .assign(year=ts.index.year)
+        .assign(month=ts.index.month)
+        .groupby(["year", "month"])
+        .sum()
+        .reset_index()
+        .pivot_table(index="month", columns="year", values=col_name)
+    ).plot(marker="o", ax=ax)
+    x_ticks = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+    plt.xticks(range(1, 13), x_ticks)
+    plt.grid(True, alpha=0.7)
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
     # Put a legend to the right of the current axis
-    ax.legend(loc='center left', bbox_to_anchor=(1.01, 0.5))
+    ax.legend(loc="center left", bbox_to_anchor=(1.01, 0.5))
     ax.set_title("Seasonal plot")
-    return f,ax
+    return f, ax
 
 
-def lag_plot(series,lags=5):
-    '''
+def lag_plot(series, lags=5):
+    """
     returns pair plot for the time series 
     lagged upto the required number.
     Please dont overkill the purpose
@@ -74,12 +91,12 @@ def lag_plot(series,lags=5):
     -----------------
     
     fig , ax object
-    '''
+    """
 
-    ##TODO: Make a function which is line plot 
+    ##TODO: Make a function which is line plot
     ## rather thab correlation plot
-    ## NOT SURE how to interpret that plot 
-    df = pd.DataFrame(series,columns=['Yt'])
-    for lag in range(1,lags+1):
-        df['lag_{}'.format(lag)] = df['Yt'].shift(lag)
+    ## NOT SURE how to interpret that plot
+    df = pd.DataFrame(series, columns=["Yt"])
+    for lag in range(1, lags + 1):
+        df["lag_{}".format(lag)] = df["Yt"].shift(lag)
     return sns.pairplot(df)
--- utils/ease.py
+++ utils/ease.py
@@ -6,17 +6,20 @@ easier.
 @author : shivam pundir
 @date   : 8 Aug 2020
 """
-__all__ = ['convert_r_dataframe_to_python']
+__all__ = ["convert_r_dataframe_to_python"]
 
 import pandas as pd
 
+
 def convert_r_dataframe_to_python(df):
-    yr_ratios  = pd.np.sort((df.time - df.time.astype(int)).unique())
-    df['year']  = df.time.astype(int)
-    df['year_fraction'] = df.time - df.year
-    df['month'] = 0
-    
-    for yr,adj in zip(yr_ratios,range(1,13)):
-        df.loc[df.year_fraction.isin([yr]),'month'] = adj        
-    df['date'] = pd.to_datetime(df.year.astype(str) + '-' + df.month.astype(str),format="%Y-%m")
-    return df.drop(['year','year_fraction','month','time'],axis=1)
+    yr_ratios = pd.np.sort((df.time - df.time.astype(int)).unique())
+    df["year"] = df.time.astype(int)
+    df["year_fraction"] = df.time - df.year
+    df["month"] = 0
+
+    for yr, adj in zip(yr_ratios, range(1, 13)):
+        df.loc[df.year_fraction.isin([yr]), "month"] = adj
+    df["date"] = pd.to_datetime(
+        df.year.astype(str) + "-" + df.month.astype(str), format="%Y-%m"
+    )
+    return df.drop(["year", "year_fraction", "month", "time"], axis=1)
